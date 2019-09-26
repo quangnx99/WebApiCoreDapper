7a53a212a73db13510c9f70818b66b49488be491
Quang Nguyễn <adquang199x@gmail.com>
2019-09-26 15:01:40 +0700

    Add project files.

---
+++ WebApiCoreDapper/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WebApiCoreDapper.Models;
+
+namespace WebApiCoreDapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly string db;
+        public ProductController(IConfiguration configuration)
+        {
+            db = configuration.GetConnectionString("DbConnectionString");
+        }
+        // GET: api/Product
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            using (var conect=new SqlConnection(db))
+            {
+                if(conect.State==System.Data.ConnectionState.Closed)
+                {
+                    conect.Open();
+                }
+                var result = await conect.QueryAsync<Product>("select Id,Sku,Price,DiscountPrice,ViewCount,ImageUrl,IsActive,CreatedDate from Products", null, null, null, System.Data.CommandType.Text);
+                return result;
+
+            }
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Product/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
