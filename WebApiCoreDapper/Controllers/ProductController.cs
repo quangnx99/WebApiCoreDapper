280f741cf67dbe7deb80241d2837619726d6c730
Quang Nguyễn <adquang199x@gmail.com>
2019-10-12 10:40:46 +0700

    commit product

--- WebApiCoreDapper/Controllers/ProductController.cs
+++ WebApiCoreDapper/Controllers/ProductController.cs
@@ -7,6 +7,7 @@ using Dapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using WebApiCoreDapper.Dtos;
 using WebApiCoreDapper.Models;
 
 namespace WebApiCoreDapper.Controllers
@@ -30,7 +31,7 @@ namespace WebApiCoreDapper.Controllers
                 {
                     conect.Open();
                 }
-                var result = await conect.QueryAsync<Product>("select Id,Sku,Price,DiscountPrice,ViewCount,ImageUrl,IsActive,CreatedDate from Products", null, null, null, System.Data.CommandType.Text);
+                var result = await conect.QueryAsync<Product>("Get_Product_All", null, null, null, System.Data.CommandType.StoredProcedure);
                 return result;
 
             }
@@ -38,27 +39,110 @@ namespace WebApiCoreDapper.Controllers
 
         // GET: api/Product/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public async Task<Product> Get(int id)
         {
-            return "value";
+            using (var conect = new SqlConnection(db))
+            {
+                if (conect.State == System.Data.ConnectionState.Closed)
+                {
+                    conect.Open();
+                }
+                var parameter = new DynamicParameters();
+                parameter.Add("@id", id);
+                var result = await conect.QueryAsync<Product>("GetProductById",parameter,null, null, System.Data.CommandType.StoredProcedure);
+                return result.Single();
+
+            }
+        }
+         // GET: api/Product/5
+        [HttpGet(Name = "GetPaging")]
+        public async Task<PagedResult> GetPaging(string keyword,int categoryId,int pageIndex,int pageSize)
+        {
+            using (var conect = new SqlConnection(db))
+            {
+                if (conect.State == System.Data.ConnectionState.Closed)
+                {
+                    conect.Open();
+                }
+                var parameter = new DynamicParameters();
+                parameter.Add("@keyword", keyword);
+                parameter.Add("@categoryId", categoryId);
+                parameter.Add("@pageIndex", pageIndex);
+                parameter.Add("@pageSize", pageSize);
+                parameter.Add("@totalRow", dbType: System.Data.DbType.Int32, direction: System.Data.ParameterDirection.Output);
+                var result = await conect.QueryAsync<Product>("Get_Product_AllPaging",parameter,null, null, System.Data.CommandType.StoredProcedure);
+                var totalRow = parameter.Get<int>("totalRow");
+                var pagedResult = new PagedResult
+                {
+                    Items = result.ToList(),
+                    TotalRow = totalRow,
+                    PageIndex = pageIndex,
+                    PageSize = pageSize
+                };
+                return pagedResult;
+
+            }
         }
 
         // POST: api/Product
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<int> Post([FromBody] Product product)
         {
+            var newId = 0;
+            using (var conect = new SqlConnection(db))
+            {
+                if (conect.State == System.Data.ConnectionState.Closed)
+                {
+                    conect.Open();
+                }
+                var parameter = new DynamicParameters();
+                parameter.Add("@sku", product.Sku);
+                parameter.Add("@price", product.Price);
+                parameter.Add("@imageUrl", product.ImageUrl);
+                parameter.Add("@isActive", product.IsActive);
+                //báo rằng trong thủ tục này có 1 biến output là id(tức để lấy ra id)
+                parameter.Add("@id",dbType:System.Data.DbType.Int32,direction:System.Data.ParameterDirection.Output);
+                var result=await conect.ExecuteAsync("Create_Product", parameter, null, null, System.Data.CommandType.StoredProcedure);
+
+                newId = parameter.Get<int>("@id");
+            }
+            return newId;
         }
 
         // PUT: api/Product/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task Put(int id, [FromBody] Product product)
         {
+            using (var conect = new SqlConnection(db))
+            {
+                if (conect.State == System.Data.ConnectionState.Closed)
+                {
+                    conect.Open();
+                }
+                var parameter = new DynamicParameters();
+                parameter.Add("@id", id);
+                parameter.Add("@sku", product.Sku);
+                parameter.Add("@price", product.Price);
+                parameter.Add("@imageUrl", product.ImageUrl);
+                parameter.Add("@isActive", product.IsActive);
+                await conect.ExecuteAsync("Update_Product", parameter, null, null, System.Data.CommandType.StoredProcedure);
+            }
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task Delete(int id)
         {
+            using (var conect = new SqlConnection(db))
+            {
+                if (conect.State == System.Data.ConnectionState.Closed)
+                {
+                    conect.Open();
+                }
+                var parameter = new DynamicParameters();
+                parameter.Add("@id", id);
+                await conect.ExecuteAsync("Delete_Product", parameter, null, null, System.Data.CommandType.StoredProcedure);
+            }
         }
     }
 }
---
+++ WebApiCoreDapper/Dtos/PagedResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiCoreDapper.Models;
+
+namespace WebApiCoreDapper.Dtos
+{
+    public class PagedResult
+    {
+        public List<Product> Items{ get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int TotalRow { get; set; }
+    }
+}
